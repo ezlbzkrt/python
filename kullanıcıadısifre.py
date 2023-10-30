73a2104b7c3efe4257dab4483c0db4f80cfb250b
LotusCake <149182020+lotuscakee@users.noreply.github.com>
2023-10-30 23:02:30 +0300

    Update kullanıcıadısifre.py

--- kullanıcıadısifre.py
+++ kullanıcıadısifre.py
@@ -1,6 +1,6 @@
 username=input('username:')
 password=input('password:')
-if username=='Ezel' and password=='04072002':
+if username=='Ezel' and password=='12345':
     print('Welcome to the system')
 else:
     print('Wrong Data')
